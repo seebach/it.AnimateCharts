e0d5f30dbefd3c1926a215a44a99eacd942e9426
seebach <yeah@seebach.dk>
2014-10-31 14:28:01 +0100

    big update
    Its actually able to stop now, and handle numbers

--- it.AnimateCharts/com-itelligence-animate.js
+++ it.AnimateCharts/com-itelligence-animate.js
@@ -6,7 +6,7 @@
 define(["jquery","./com-itelligence-animate-properties"], 
 		function($,properties) { 
 	    var runCount = 1;
-
+	    var timer;
 		return { 
 			type : "Animates",
 			//Refer to the properties file
@@ -23,123 +23,102 @@ define(["jquery","./com-itelligence-animate-properties"],
 					}]
 				},
 			},
+			
 			paint : function($element, layout) { 
-				if(!_this) {
-					log('init this');
-					var _this = this;	
-				}
+
 				var valueArray = [];
 				var i = 0;
-				//log (_this.arrayLength);
-				//log (_this.valueArray);
+				var _this = this;
 
 				runCount = runCount+1;
 				log('start paint count '+runCount);
-				// find out if this is the first run
-/*				log(_this.running);
-				if(_this.running===1) {
-					log ('re-build exit');
-					i = this.i; 
-					var	valueArray = this.valueArray;
-			
-				} else {
-					log ('build');
-					i = 0;
-					var valueArray = new Array();			
-				}				
-*/
-				// check content of div
-				log('build html');
+
+				// start button
 				var button = document.createElement("input");
 				    button.type = "submit";
-				    button.value = "run it";
+				    button.value = "Start Sequence";
 				    button.addClass = "runIt";
 				    button.onclick = function () {
 						arrayLength = valueArray.length;
 						log ('run');
 						// store values for later
-						_this.running = 1;  
-						_this.valueArray = valueArray;
 
-						animate(i,valueArray,arrayLength);
+						log(animate);
+						animate(i,valueArray,arrayLength)
+						;
+
+						// , ,valueArray,arrayLength);
 				   
-				}
-				 $element.html(button);
-				
-				if( this.running != 1) {
+				}				 
+				 // stop button
+				var button_stop = document.createElement("input");
+				    button_stop.type = "submit";
+				    button_stop.value = "Stop Sequence";
+				    button_stop.addClass = "stop It";
+				    button_stop.onclick = function () {
+						log ('stop it');
+						animateStop();
+				} 
+
 				//check that we have data to render
 				if(layout.qHyperCube.qDataPages[0].qMatrix.length>=1 ) { 
 					log('we have dims');
 					var dimensions = layout.qHyperCube.qDataPages[0].qMatrix.length;	
 					// || layout.qHyperCube.qDataPages[0].qMatrix.length) {	
 					var dimName = layout.qHyperCube.qDimensionInfo[0].qFallbackTitle;
-					var dimId = layout.qHyperCube.qDimensionInfo[0].cId;
-					log( dimName +' '+ dimId);
-
 					var count = 0;
-					var salt = Math.round( Math.random() *10000);
+					
 					var qData = layout.qHyperCube.qDataPages[0];
 
 					$.each(qData.qMatrix, function(key, row) {  						
 				
 						// log loop into rows
 						$.each(row, function(index, cell) {
-							
-						//	log(row[0].qText);
-							
-							if(isNaN(row[0].qText)===false) {
+							// handle text values
+							if(isNaN(row[0].qText)) {
 								valueArray.push(row[0].qText);
-								log (row[0].qText);
-							} 	
-							if(isNaN(row[0].qNum)) {
+								log ('text'+row[0].qText);
+							} 
+							// handle numbers
+							if(isNaN(row[0].qNum)===false) {
 								valueArray.push(row[0].qNum);
 								log (row[0].qNum);
 							} 	
-							if(row[0].qIsOtherCell===true) {
-								// other label needs tp be set dynamically
-								valueArray.push =  layout.qHyperCube.qDimensionInfo[0].othersLabel;
-							}
 						});
 						count = count+1;							
 					});			
 				}
 				log(valueArray);
+
+				// debug values parsed
 				var div = document.createElement("div");
 				    div.id = "animateId";
 					div.textContent = valueArray;
-				    
+				
+				//add to HTML
+				$element.html(button);
+				$element.append(button_stop);    
 				$element.append(div);
 
-
-}
-
-				function animate (i,valueArray,arrayLength) { 
-					        //  create a loop function
-				   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-				      //alert('hello');          //  your code here
-//					     	log(this.getAttribute("data-value"));
-//						      var value = parseInt(this.getAttribute("data-value"), i );
-//						      var arr = Array(parseInt(i));
-					    log('setTimeout loop '+i);
-					    log(this);
-					    log(valueArray[i]);
-					    //	_this.backendApi.clearSelections();
-					      _this.backendApi.selectValues(0,[i],false);
-//						      this.backendApi.selectValues(0,['jan'] , true);
-				      // }
-//					      $element.html(valueArray[i]);
-				     // log(valueArray[i]);
-					   i++;
-				      _this.i = i;                     //  increment the counter
-				      if (i < arrayLength) { 
-				      	log(i);
-				      	log(arrayLength);           //  if the counter < 10, call the loop function
-				        animate(i,valueArray,arrayLength);             //  ..  again which will trigger another 
-				      } else {
-						this.running = 0;
-				      }                        //  ..  setTimeout()
-				   }, 500)
-				}
+			function animate (i,valueArray,arrayLength) {
+				// var i,valueArray,arrayLength;
+				        //  create a loop function
+			   timer = setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+				    log('setTimeout loop '+i);
+				    log(valueArray[i]);
+				      _this.backendApi.selectValues(0,[i],false);
+				   i++;
+			      _this.i = i;                     //  increment the counter
+			      if (i < arrayLength) { 
+			      	log(i);
+			      	log(arrayLength);           //  if the counter < 10, call the loop function
+			        animate(i,valueArray,arrayLength);            //  ..  again which will trigger another 
+			      }                      //  ..  setTimeout()
+			   }, 1500)
+			}
+			function animateStop () {
+				clearTimeout(timer);
+			}
 
 				 // i use this logging function so its easy to turn logging on or off
 				 function log(obj) {
